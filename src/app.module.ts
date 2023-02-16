ef04467b8464af9dab23b20f40240d60b5d7a958
Minhaj ur Rehman <115610484+minhajDev@users.noreply.github.com>
2023-02-16 16:24:09 +0500

    Add mongoose package and connect DB.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
+const DB_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@learning-nestjs.dmncmuk.mongodb.net/?retryWrites=true&w=majority`;
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot(DB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
