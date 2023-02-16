5b3b164095ae7a9f14ae84820d43e02ec7d38257
Minhaj ur Rehman <115610484+minhajDev@users.noreply.github.com>
2023-02-16 15:26:13 +0500

    initial commit.

---
+++ src/products/products.module.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { ProductsService } from './product.service';
+import { ProductsController } from './products.controller';
+
+@Module({
+  controllers: [ProductsController],
+  providers: [ProductsService],
+})
+export class ProductsModule {}
