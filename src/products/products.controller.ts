9de69d86c15f5cf9d1a8dce698ac0f3fb573a12d
Minhaj ur Rehman <115610484+minhajDev@users.noreply.github.com>
2023-02-17 14:19:00 +0500

    Refactor.

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,11 +2,14 @@
 import {
   Body,
   Controller,
-  Param,
-  Post,
-  Get,
-  Patch,
   Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
 } from '@nestjs/common';
 import { ProductsService } from './product.service';
 
@@ -21,53 +24,72 @@ export class ProductsController {
     @Body('desc') prodDesc: string,
     @Body('price') prodPrice: number,
   ) {
-    // Calls the ProductService to insert the new product and returns the ID
     const generatedId = await this.productsService.insertProduct(
       prodTitle,
       prodDesc,
       prodPrice,
     );
-    return { id: generatedId };
+    // return success message with the id of the newly created product
+    return { id: generatedId, message: 'Product added successfully' };
   }
 
   // Handles incoming HTTP GET requests to retrieve all products
   @Get()
   async getAllProducts() {
-    // Calls the ProductService to get all products and returns them in a simplified format
     const products = await this.productsService.getProducts();
-    return products.map((prod) => ({
-      id: prod.id,
-      title: prod.title,
-      desc: prod.desc,
-      price: prod.price,
-    }));
+    // return array of products with count
+    return {
+      count: products.length,
+      products: products.map((prod) => ({
+        id: prod.id,
+        title: prod.title,
+        desc: prod.desc,
+        price: prod.price,
+      })),
+    };
   }
 
   // Handles incoming HTTP GET requests to retrieve a single product by ID
   @Get(':id')
-  getProduct(@Param('id') prodId: string) {
-    // Calls the ProductService to get the single product by ID and returns it
-    return this.productsService.getSingleProduct(prodId);
+  async getProduct(@Param('id') prodId: string) {
+    const product = await this.productsService.getSingleProduct(prodId);
+    // return the requested product
+    return {
+      id: product.id,
+      title: product.title,
+      desc: product.desc,
+      price: product.price,
+    };
   }
 
   // Handles incoming HTTP PATCH requests to update an existing product
   @Patch(':id')
-  updateProduct(
+  async updateProduct(
     @Param('id') prodId: string,
     @Body('title') prodTitle: string,
     @Body('desc') prodDesc: string,
     @Body('price') prodPrice: number,
   ) {
-    // Calls the ProductService to update the existing product with the new data
-    this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
-    return null;
+    await this.productsService.updateProduct(
+      prodId,
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    );
+    // return success message
+    return { message: 'Product updated successfully' };
   }
 
   // Handles incoming HTTP DELETE requests to remove a product by ID
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   async removeProduct(@Param('id') prodId: string) {
-    // Calls the ProductService to delete the product with the given ID
-    await this.productsService.deleteProduct(prodId);
-    return null;
+    const result = await this.productsService.deleteProduct(prodId);
+    if (result.deletedCount === 0) {
+      // if the product was not found, throw an error
+      throw new NotFoundException('Could not find product.');
+    }
+    // return success message
+    return { message: 'Product deleted successfully' };
   }
 }
